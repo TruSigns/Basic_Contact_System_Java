4a44fa4a282b246f1a1b5ba2b370a20ca1283fa1
TruSigns <ReeseRuffin>
2022-02-02 13:15:11 -0500

    bug fix

--- src/cademy/training/MobilePhone.java
+++ src/cademy/training/MobilePhone.java
@@ -31,7 +31,15 @@ public class MobilePhone {
         if(foundPosition <0){
             System.out.println(oldContact.getName() + " was not found");
             return false;
+        }else if(findContact(newContact.getName()) != -1){
+            System.out.println("Contact with name "  + newContact.getName() + "already exist");
+
+
+
+
+
         }
+
 
         this.myContacts.set(foundPosition, newContact);
         System.out.println(newContact.getName() + " Replaced with " + newContact.getName());
